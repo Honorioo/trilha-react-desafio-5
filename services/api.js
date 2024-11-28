7f4ac6614d5ecae4f9590b92dfb258067f92749c
Honorioooo <viniciusgomeshonorio@hotmail.com>
2024-11-28 09:55:37 -0300

    Desfio Concluido

--- services/api.js
+++ services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const api  = axios.create({
     baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlsd3ZiY2dpcWt0ZWVkcnNpamZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3OTU2OTIsImV4cCI6MjA0ODM3MTY5Mn0.ujVg1YlU9WgvnJB2j7Z0VQo9dzfqdRSWx7irS6JkN6Q",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlsd3ZiY2dpcWt0ZWVkcnNpamZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3OTU2OTIsImV4cCI6MjA0ODM3MTY5Mn0.ujVg1YlU9WgvnJB2j7Z0VQo9dzfqdRSWx7irS6JkN6Q"
     }
 })
